d51199e4c5c2a68d26604e7c192186604ddeec70
Rafael Tackla <rafaeltackla@outlook.com>
2019-07-04 12:16:36 -0300

    organizado importações

--- backend/src/test/java/com/pag/backend/repository/CustomerRepositoryTests.java
+++ backend/src/test/java/com/pag/backend/repository/CustomerRepositoryTests.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.pag.backend.builders.CustomerBuilder;
-import com.pag.backend.domain.Customer;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerRepositoryTests {
