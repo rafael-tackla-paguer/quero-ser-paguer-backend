0cdd4ad22d4f0ffe29a84eb41983be0ef70e4358
rafae <rafae@DESKTOP-LF52LRV>
2019-07-03 23:32:15 -0300

    criação do método OrderRepository.java 	retornar todos os pedidos de um determinado cliente

--- backend/src/main/java/com/pag/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/pag/backend/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.pag.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pag.backend.domain.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	public List<Order> findAllByCustomerId(Long id);
 }
