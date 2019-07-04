e887d9131386a752626a25bfb9b04a76707e70b6
rafae <rafae@DESKTOP-LF52LRV>
2019-07-04 00:40:52 -0300

    utilização do builder de cliente

--- backend/src/test/java/com/pag/backend/repository/CustomerRepositoryTests.java
+++ backend/src/test/java/com/pag/backend/repository/CustomerRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.pag.backend.repository;
 
+import static com.pag.backend.builders.CustomerBuilder.oneCustomer;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.pag.backend.builders.CustomerBuilder;
 import com.pag.backend.domain.Customer;
 
 @RunWith(SpringRunner.class)
@@ -18,13 +21,13 @@ public class CustomerRepositoryTests {
 	
 	@Before
 	public void setup() {
-		Customer customer = new Customer();
+		
 	}
 	
 	@Test
 	public void saveTest() {
 		//cenário
-		
+		oneCustomer().now();
 		//ação
 		
 		//verificação
