cd1fc140e9aa644505d674a1fcccaa004c365cf1
rafae <rafae@DESKTOP-LF52LRV>
2019-07-05 16:43:14 -0300

    atualização dos resources rest

--- backend/src/main/java/com/pag/backend/controller/rest/OrderItemResource.java
+++ backend/src/main/java/com/pag/backend/controller/rest/OrderItemResource.java
@@ -1,9 +1,19 @@
 package com.pag.backend.controller.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pag.backend.domain.OrderItem;
 import com.pag.backend.service.OrderItemService;
 
 @RestController
@@ -12,4 +22,29 @@ public class OrderItemResource {
 	
 	@Autowired
 	private OrderItemService service;
+	
+	@PostMapping
+	public ResponseEntity<OrderItem> save(@RequestBody OrderItem orderItem){
+		OrderItem createdOrderItem = service.save(orderItem);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdOrderItem);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<OrderItem> update(@PathVariable(name = "id") Integer id, @RequestBody OrderItem orderItem){
+		orderItem.setId(id);
+		OrderItem updatedOrderItem = service.update(orderItem);
+		return ResponseEntity.ok(updatedOrderItem);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<OrderItem> findById(@PathVariable("id") Integer id) {
+		OrderItem orderItem = service.findById(id);
+		return ResponseEntity.ok(orderItem);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<OrderItem>> findAll(){
+		List<OrderItem> orderItems = service.findAll();
+		return ResponseEntity.ok(orderItems);
+	}
 }
--- backend/src/main/java/com/pag/backend/controller/rest/OrderResource.java
+++ backend/src/main/java/com/pag/backend/controller/rest/OrderResource.java
@@ -1,9 +1,19 @@
 package com.pag.backend.controller.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pag.backend.domain.Order;
 import com.pag.backend.service.OrderService;
 
 @RestController
@@ -12,4 +22,30 @@ public class OrderResource {
 
 	@Autowired
 	private OrderService service;
+	
+	@PostMapping
+	public ResponseEntity<Order> save(@RequestBody Order order){
+		Order createdOrder = service.save(order);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdOrder);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Order> update(@PathVariable(name = "id") Integer id, @RequestBody Order order){
+		order.setId(id);
+		Order updatedOrder = service.update(order);
+		return ResponseEntity.ok(updatedOrder);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Order> findById(@PathVariable("id") Integer id) {
+		Order order = service.findById(id);
+		return ResponseEntity.ok(order);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll(){
+		List<Order> orders = service.findAll();
+		return ResponseEntity.ok(orders);
+	}
+	
 }
