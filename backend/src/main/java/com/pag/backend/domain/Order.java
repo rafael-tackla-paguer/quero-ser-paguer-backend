3f05f119fe86157522c8eafbc39e3231f6b6fc4c
Rafael Tackla <rafaeltackla@outlook.com>
2019-07-10 11:42:23 -0300

    fecth eager order > orderItems relationship

--- backend/src/main/java/com/pag/backend/domain/Order.java
+++ backend/src/main/java/com/pag/backend/domain/Order.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -43,7 +44,7 @@ public class Order implements Serializable{
 	@Column(name = "valor", precision = 10, scale = 2)
 	private BigDecimal value;
 	
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
 	@JsonIgnoreProperties("order")
 	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 }
